01137b888a398316d01475dc03592d3a0b1972be
Yurii Kucher <ukucher@gmail.com>
2024-04-05 16:13:59 +0300

    lesson2 is done

--- src/features/Movies/MovieCard.tsx
+++ src/features/Movies/MovieCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import styles from "./MovieCard.module.scss";
 
 interface MovieCardProps {
   id: number;
@@ -9,10 +10,19 @@ interface MovieCardProps {
 
 export function MovieCard({ id, title, overview, popularity }: MovieCardProps) {
   return (
-    <div className="Movies-card">
-      <Link to={`/movies/${id}`}>{title}</Link>
-      <div className="Movies-card-overview">{overview}</div>
-      <div className="Movies-card-pop">{popularity}</div>
+    <div className={styles.card}>
+      <img
+        className={styles.thumbnail}
+        src="/movie-thumb.png"
+        alt="Movie thumbnail"
+      />
+      <div className={styles.content}>
+        <div>
+          <Link to={`/movies/${id}`}>{title}</Link>
+        </div>
+        <div className={styles.overview}>{overview}</div>
+        <div className={styles.popularity}>{popularity}</div>
+      </div>
     </div>
   );
 }
--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { RootState } from "../../store";
 import { MovieCard } from "./MovieCard";
 
-import "./Movies.css";
+import styles from "./Movies.module.scss";
 
 interface MoviesProps {
   movies: Movie[];
@@ -12,7 +12,7 @@ interface MoviesProps {
 function Movies({ movies }: MoviesProps) {
   return (
     <section>
-      <div className="Movies-list">
+      <div className={styles.list}>
         {movies.map((m) => (
           <MovieCard
             key={m.id}
